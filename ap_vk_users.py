ef8128281cae87a000c86f82d8dbaf3aa0358624
Kirill Shcherbakov <scherbakovkirill@yandex.ru>
2024-04-06 15:31:51 +0300

    correct2

---
+++ ap_vk_users.py
@@ -0,0 +1,83 @@
+import requests
+from pprint import pprint
+from Constant import TOKEN_USER
+
+
+class VK_Users:
+    def __init__(self, TOKEN_USER):
+        self.URL_API = 'https://api.vk.com/method'
+        self.TOKEN_USER = TOKEN_USER
+
+
+    def data_users(self, sex, city, year):
+        params = {
+            'access_token': self.TOKEN_USER,
+            'sort': 0,
+            'count': 200,
+            'has_photo': 1,
+            'v': 5.199,
+            'fields': 'city,sex,counters',
+            'birth_year': year,
+            'city': city,  # идентификатор города
+            'sex': sex
+        }
+        data_users = []
+        response = requests.get(f'{self.URL_API}/users.search', params=params)
+        data = response.json()['response']['items']
+        for user in data:
+            try:
+                if user['sex'] == sex and user['city']['id'] == 2 :
+                    data_users.append(user['id'])
+                    # print('ok')
+            except:
+                pass
+        return data_users
+
+
+    def get_user_info(self, user_vk):
+        params = {
+            'access_token': self.TOKEN_USER,
+            'user_ids': user_vk,
+            'fields': 'city,sex,bdate',
+            'v': 5.199
+            }
+        response = requests.get(f'{self.URL_API}/users.get', params=params)
+        user_info = response.json()['response'][0]
+        user_info = [user_info['id'], user_info['first_name'], user_info['last_name'],
+                     user_info['city']['id'], user_info['sex'], user_info['bdate'][5:]]
+        return user_info
+
+
+    def photos_user(self, user_vk):
+        params = {
+            'access_token': self.TOKEN_USER,
+            'user_id': user_vk,
+            'extended': 1,
+            'v': 5.199
+            }
+        photos = {}
+        like = 0
+        response = requests.get(f'{self.URL_API}/photos.getUserPhotos', params=params)
+        user_photos = response.json()['response']['items']
+        for photo in user_photos:
+             photos.update({photo['likes']['count']: photo['sizes'][-2]['url']})
+        sort_photos = sorted(photos.items())
+        photos = sort_photos[-3:]
+        return  (photos[0][1], photos[1][1], photos[2][1])
+
+
+
+# ap = VK_Users(TOKEN_USER)
+# sex = 1
+# city = 2
+# year = 1986
+# data_qoest = ap.data_users(sex, city, year)
+# pprint(data_qoest)
+# user_vk = 711878878
+# user_vk = 809529828
+# print(ap.get_user_info(user_vk))
+# pprint(data_qoest[0])
+# user_vk = 567469497
+# api.count_photos(data_qoest)
+# pprint(ap.photos_user(user_vk))
+
---
+++ vk_bot.py
@@ -0,0 +1,55 @@
+import random
+from random import randrange
+from Constant import TOKEN_GR, TOKEN_USER, TOKEN_GR2, TOKEN_GR3
+import vk_api
+from vk_api.longpoll import VkLongPoll, VkEventType
+from ap_vk_users import VK_Users
+from base import Metod
+
+vk = vk_api.VkApi(token=TOKEN_GR)
+longpoll = VkLongPoll(vk)
+ap = VK_Users(TOKEN_USER)
+bd = Metod()
+
+
+def write_msg(user_id, message):
+    vk.method('messages.send', {'user_id': user_id, 'message': message,  'random_id': randrange(10 ** 7),})
+
+def show_us(): #показывает кандидата
+    # us = random.choice(DATA_US)
+    us = bd.get_user_random(us_info[0])
+    us_info = ap.get_user_info(us)
+    us_url = f'https://vk.com/id{us}'
+    write_msg(event.user_id, f'{us_info[1]} {us_info[2]}')
+    photos = ap.photos_user(g_id)
+    write_msg(event.user_id, photos[0])
+    # добавить фото
+    # добавляем в избранное или удаляем из DATA_US
+
+
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text
+            q_id = event.user_id
+            q_inf = ap.get_user_info(event.user_id)
+            print(q_inf)
+            if q_inf[4] == 2: q_sex = 1
+            else: q_sex = 2
+
+            goest_in_bd = bd.add_quests(q_inf[0]) # есть или нет в списке bd добавить гостя
+            if goest_in_bd == False:   #если гостя нет
+                write_msg(event.user_id, f"Привет, {g_inf[1]}!")
+                write_msg(event.user_id, "Формируется база подходящих тебе кандидатов....ждите")
+                DATA_US = ap.data_users(q_sex, q_inf[3], q_inf[5])
+                bd.add_users(q_inf[0],DATA_US)
+                write_msg(event.user_id, "Для просмотра отправь сообщение - 'СЛЕДУЮЩИЙ'")
+                write_msg(event.user_id, "Для просмотра избранного, отправь сообщение - 'ИЗБРАННОЕ'")
+
+            if request.upper() == 'СЛЕДУЮЩИЙ ПРОСМОТР':
+                show_us()
+
+            elif request.upper() == 'ИЗБРАННОЕ':
+                pass
+                # добавить список из базы
